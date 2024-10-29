06062b69c856d289ee22f92306482b031045c073
kersven <144127530+kersven@users.noreply.github.com>
2024-10-29 22:27:11 +0100

    Update Program.cs

--- MathAPI/Program.cs
+++ MathAPI/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddSwaggerGen();
 //builder.Services.AddDbContext<MathDbContext>(options => options.UseSqlServer("Server=sqlserver,1433;Database=mathdb;User Id=sa;Password=NotVeryStrong@Passw0rd;"));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();

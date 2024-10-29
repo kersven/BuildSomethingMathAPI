9de6183e40e83bbbabda9ce004efac1bae4c13de
kersven <144127530+kersven@users.noreply.github.com>
2024-10-29 17:51:28 +0100

    Add files via upload

---
+++ MathDb/MathDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MathDb
+{
+    public class MathDbContext:DbContext
+    {
+        public MathDbContext(DbContextOptions<MathDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}

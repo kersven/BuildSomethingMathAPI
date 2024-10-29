e1a7bb314674ce71f802b6b1011994403e5cef8b
kersven <144127530+kersven@users.noreply.github.com>
2024-10-29 22:25:17 +0100

    Update Program.cs

--- MathAPI/Program.cs
+++ MathAPI/Program.cs
@@ -3,10 +3,7 @@ using MathDb;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //builder.Services.AddDbContext<MathDbContext>(options => options.UseSqlServer("Server=sqlserver,1433;Database=mathdb;User Id=sa;Password=NotVeryStrong@Passw0rd;"));

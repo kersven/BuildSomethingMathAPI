98f06838e0d010a11d1fdc0a4e290eba6fd1ad0d
kersven <144127530+kersven@users.noreply.github.com>
2024-10-30 20:43:08 +0100

    Async och rensa

--- MathAPI/Program.cs
+++ MathAPI/Program.cs
@@ -1,12 +1,8 @@
-using Microsoft.EntityFrameworkCore;
-using MathDb;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<MathDbContext>(options => options.UseSqlServer("Server=sqlserver,1433;Database=mathdb;User Id=sa;Password=NotVeryStrong@Passw0rd;"));
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
